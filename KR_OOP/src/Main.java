dfb23307b0b1bae1d75dda0980d42e69e3c93473
ntasti <ntaasti@gmail.com>
2023-11-29 21:37:37 +0400

    л

--- KR_OOP/src/Main.java
+++ KR_OOP/src/Main.java
@@ -1,6 +1,49 @@
+import java.sql.SQLOutput;
+import java.util.*;
 
+class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        //Создание  вектора AB черeз  двумерный масив, где vector[x1=0 y1=0] по умолчанию,точку [x2 y2] вводим с клавиатуры
+        System.out.println("Координаты вектора CD X0 Y0 ={0,0} .Введите координаты X1 Y1 ");
+        int[][] vectorAB = new int[2][2];
+        vectorAB[0][0] = 0;
+        vectorAB[0][1] = 0;
+
+        for (int u = 0; u < vectorAB.length; u++) {
+            vectorAB[1][u] = scanner.nextInt();
+        }
+
+
+//Создание произвольного вектора СD черз  двумерный масив, где vector[x1 y1][x2 y2]
+        System.out.println("Введите координаты X0 Y0 и X1 Y1 для вектоар CD");
+        int[][] vectorCD = new int[2][2];
+        for (int i = 0; i < vectorCD.length; i++) {
+
+            for (int u = 0; u < vectorCD.length; u++) {
+
+                vectorCD[i][u] = scanner.nextInt();
+            }
+
+        }
+
+        //координаты вектора AB
+        System.out.println("координаты вектора AB");
+        for (int i = 0; i < vectorAB.length; i++) {
+            for (int u = 0; u < vectorAB.length; u++) {
+                System.out.print(vectorAB[i][u] + " ");
+            }
+            System.out.println(":X" + i + " Y" + i);
+        }
+
+        //координаты вектора СD
+        System.out.println("координаты вектора СD");
+        for (int i = 0; i < vectorCD.length; i++) {
+            for (int u = 0; u < vectorCD.length; u++) {
+                System.out.print(vectorCD[i][u] + " ");
+            }
+            System.out.println(":X" + i + " Y" + i);
         }
     }
 }
---
+++ KR_OOP/src/VectorFunction.java
@@ -0,0 +1,9 @@
+public class VectorFunction {
+
+    public int Prisvaivanie;
+    public int Sravnenie;
+    public int Umnozhenie;
+    public int Vivod;
+
+
+}
