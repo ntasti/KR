9e3c48cf5f97991e560c35609c244b9818b5e6d2
ntasti <ntaasti@gmail.com>
2023-11-30 21:44:20 +0400

    t

--- KR_OOP/src/Main.java
+++ KR_OOP/src/Main.java
@@ -1,63 +1,30 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class Main {
+public class Main{
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        //Создание  вектора AB черeз  двумерный масив, где vector[x1=0 y1=0] по умолчанию,точку [x2 y2] вводим с клавиатуры
-        System.out.println("Координаты вектора AB X0 Y0 ={0,0} .Введите координаты X1 Y1 ");
-        VectorFunction vectorAB = new VectorFunction();
-        vectorAB.pointVectorAB = new int[2][2];
-        vectorAB.pointVectorAB[0][0] = 0;
-        vectorAB.pointVectorAB[0][1] = 0;
-        for (int u = 0; u < vectorAB.pointVectorAB.length; u++) {
-            vectorAB.pointVectorAB[1][u] = scanner.nextInt();
-        }
-        //Вывод точек  вектора AB
-        System.out.println("координаты вектора AB");
-        for (int i = 0; i < vectorAB.pointVectorAB.length; i++) {
-            for (int u = 0; u < vectorAB.pointVectorAB.length; u++) {
-                System.out.print(vectorAB.pointVectorAB[i][u] + " ");
-            }
-            System.out.println(":X" + i + " Y" + i);
-        }
-        //Создание  вектора CD черeз  двумерный масив
-        System.out.println("Введите координаты X0 Y0 и X1 Y1 для вектоар CD");
-        VectorFunction vectorCD = new VectorFunction();
-        vectorCD.pointVectorCD = new int[2][2];
-        for (int i = 0; i < vectorCD.pointVectorCD.length; i++) {
 
-            for (int u = 0; u < vectorCD.pointVectorCD.length; u++) {
+        System.out.println("Заполнить точки вектора  клавитуры или рандомно?");
+        System.out.println("1.C клавиатуры");
+        System.out.println("2.Рандомно");
+        int data = scanner.nextInt();
+        VectorFunction vectorAB = new VectorFunction(data);
 
-                vectorCD.pointVectorCD[i][u] = scanner.nextInt();
-            }
-        }
-        // Вывод точек  вектора СD
-        System.out.println("координаты вектора СD");
-        for (int i = 0; i < vectorCD.pointVectorCD.length; i++) {
-            for (int u = 0; u < vectorCD.pointVectorCD.length; u++) {
-                System.out.print(vectorCD.pointVectorCD[i][u] + " ");
-            }
-            System.out.println(":X" + i + " Y" + i);
-        }
 
-        //Переноc данных из одного веrтора во второй
-        vectorAB.pointVectorAB = vectorAB.assignmentPoints(vectorCD.pointVectorCD);
-        //Характеристи вектора AB
-        // Модуль
-        System.out.println("Модуль вектора AB " + vectorAB.moduleVector(vectorAB.pointVectorAB));
-        //Скалярное произведение векторов
-        System.out.println("Скалярное произведение " + vectorAB.scalarProduct(vectorAB.pointVectorAB, vectorCD.pointVectorCD));
-        //Высчет и выввод координат   по точкам
-        System.out.println("Координаты вектора " + Arrays.toString(vectorAB.vectorCoordinates(vectorCD.pointVectorCD)));
-        //Вывод точек  вектора AB
-        System.out.println("координаты вектора AB");
-        for (int i = 0; i < vectorAB.pointVectorAB.length; i++) {
-            for (int u = 0; u < vectorAB.pointVectorAB.length; u++) {
-                System.out.print(vectorAB.pointVectorAB[i][u] + " ");
-            }
-            System.out.println(":X" + i + " Y" + i);
-        }
+        System.out.println("Ввведите номер функции,которую вы хотите выполнить:");
+        String text = """
+                1.Перенести данные из AB в CD
+                2.Перенести данные из  CD в AB
+                3.Модуль вектора AB и СВ
+                4.Скалярное произведение векторов AB в CD
+                5.Высчет кооринат по точкам вектора AB и СD
+                6.Все
+                """;
+        System.out.println(text);
+        int functionNumber = scanner.nextInt();
+        vectorAB.function(functionNumber);
     }
 }
+
--- KR_OOP/src/VectorFunction.java
+++ KR_OOP/src/VectorFunction.java
@@ -1,24 +1,143 @@
-public class VectorFunction {
+import java.util.*;
+import java.util.Arrays;
 
-    public int[][] pointVectorAB;
-    public int[][] pointVectorCD;
+public class VectorFunction {
+    Scanner scanner = new Scanner(System.in);
+    public int[][] pointVectorAB = {{0, 0}, {1, 1}};
+    public int[][] pointVectorCD = new int[2][2];
     public int[] coordinateAB = new int[2];
     public int[] coordinateCD = new int[2];
+    private int data;
 
-    public void setValues(int[][] pointVectorAB, int[][] pointVectorCD) {
-        pointVectorAB = this.pointVectorAB;
-        pointVectorCD = this.pointVectorCD;
-
+    //Конструктор
+    public VectorFunction(int data) {
+        this.data = data;
+        InitializationData(data);
     }
 
-//    public String getValues() {
-//        return "";
-//    }
+    //Инициализация
+    public void InitializationData(int data) {
+        switch (data) {
+            case 1:
+                System.out.println("Координаты вектора AB X0 Y0 ={0,0} .Введите координаты X1 Y1 ");
+                pointVectorAB = OutputCoordinateAB(pointVectorAB);
+                System.out.println("координаты вектора AB");
+                Output(pointVectorAB);
+                System.out.println("Введите координаты X0 Y0 и X1 Y1 для вектоар CD");
+                pointVectorCD = OutputCoordinateCD(pointVectorCD);
+                System.out.println("координаты вектора CD");
+                Output(pointVectorCD);
+
+                break;
+            case 2:
+                pointVectorAB = OutputCoordinateRandomAB(pointVectorAB);
+                System.out.println("координаты вектора AB");
+                Output(pointVectorAB);
+                pointVectorCD = OutputCoordinateRandomCD(pointVectorCD);
+                System.out.println("координаты вектора CD");
+                Output(pointVectorCD);
+                break;
+        }
+    }
+
+    //Вывод точек вектора
+    public void Output(int[][] pointVector) {
+        for (int i = 0; i < pointVector.length; i++) {
+            for (int u = 0; u < pointVector.length; u++) {
+                System.out.print(pointVector[i][u] + " ");
+            }
+            System.out.println(":X" + i + " Y" + i);
+        }
+    }
+
+    //Заполнение с клавиатуры AB
+    public int[][] OutputCoordinateAB(int[][] pointVector) {
+        for (int u = 0; u < pointVector.length; u++) {
+
+            pointVector[1][u] = scanner.nextInt();
+        }
+        return pointVector;
+    }
+
+    //Заполнение с клавиатуры CD
+    public int[][] OutputCoordinateCD(int[][] pointVector) {
+        for (int i = 0; i < pointVector.length; i++) {
+
+            for (int u = 0; u < pointVector.length; u++) {
+
+                pointVector[i][u] = scanner.nextInt();
+            }
+        }
+        return pointVector;
+    }
+
+    //Заполнение рандом AB
+    public int[][] OutputCoordinateRandomAB(int[][] pointVector) {
+        Random random = new Random();//функция рандом
+        for (int u = 0; u < pointVector.length; u++) {
+            pointVector[1][u] = random.nextInt(100);//заполнение рандомными числами до 100
+        }
+        return pointVector;
+    }
+
+    //Заполнение рандом СD
+    public int[][] OutputCoordinateRandomCD(int[][] pointVector) {
+        Random random = new Random();//функция рандом
+        for (int i = 0; i < pointVector.length; i++) {
+            for (int u = 0; u < pointVector.length; u++)
+                pointVector[i][u] = random.nextInt(100);//заполнение рандомными числами до 100
+        }
+        return pointVector;
+    }
+
+    ////ФУНКЦИИ
+    public void function(int function) {
+        switch (function) {
+            case 1:
+                System.out.println("Присвоить данные вектора AB вектору CD:");
+                assignmentPoints(pointVectorAB);
+                Output(pointVectorAB);
+                break;
+            case 2:
+                System.out.println("Присвоить данные вектора  CD в AB:");
+                pointVectorCD = assignmentPoints(pointVectorAB);
+                Output(pointVectorCD);
+                break;
+            case 3:
+                System.out.println("Модуль вектора AB и СD:");
+                pointVectorAB = assignmentPoints(pointVectorCD);
+                System.out.println("Модуль АВ: " + moduleVector(pointVectorAB));
+                System.out.println("Модуль СD: " + moduleVector(pointVectorCD));
+                break;
+            case 4:
+                System.out.println("Скалярное произведение векторов AB в CD:");
+                System.out.println(scalarProduct(pointVectorAB, pointVectorCD));
+                break;
+            case 5:
+                System.out.println("Высчет кооринат по точкам вектора AB и СD:");
+                coordinateAB = vectorCoordinates(pointVectorAB);
+                coordinateCD = vectorCoordinates(pointVectorCD);
+                System.out.println("Координаты AB:" + Arrays.toString(coordinateAB));
+                System.out.println("Координаты СD:" + Arrays.toString(coordinateCD));
+                break;
+            case 6:
+                System.out.println("Все,кроме присвоить:");
+                System.out.println("Модуль АВ: " + moduleVector(pointVectorAB));
+                System.out.println("Модуль СD: " + moduleVector(pointVectorCD));
+                System.out.println("Скалярное произведение векторов AB в CD:" + scalarProduct(pointVectorAB, pointVectorCD));
+                coordinateAB = vectorCoordinates(pointVectorAB);
+                coordinateCD = vectorCoordinates(pointVectorCD);
+                System.out.println("Координаты АВ:" + Arrays.toString(coordinateAB));
+                System.out.println("Координаты СD:" + Arrays.toString(coordinateCD));
+                break;
+        }
+    }
 
     //Присваивание
     public int[][] assignmentPoints(int vector1[][]) {
+
         int vector[][] = vector1;
-       return vector;
+        return vector;
     }
 
     //высчет модуля вектора 2 пункт
@@ -42,6 +161,4 @@ public class VectorFunction {
         coordinate[1] = vector[1][1] - vector[0][1];
         return coordinate;
     }
-
-
 }
